75f2dd24ca81b229168ec10d9fed6203b43109ad
Cole Lewis <cole.g.lewis@gmail.com>
2022-07-27 18:28:48 -0400

    Logging and documentation changes.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # pocket-cors-proxy
+
+## Usage
+
+### Server-Side
+* `git clone https://github.com/colelewis/pocket-cors-rss-proxy`
+* `cd pocket-cors-rss-proxy/`
+* `node index.js`
+* Open localhost:8001/source/*your RSS URL* in a new tab in your web browser.
+
+### Client-Side
+The server returns an [XMLDocument](https://developer.mozilla.org/en-US/docs/Web/API/XMLDocument) element. 
+Fetch calls can be made to retrieve and use the XML data as follows:
+
+```
+fetch('SERVER_IP/source/' + RSS_URL)
+      .then(data => data.json())
+      .then(contents => contents.rawHTML)
+      .then(runner => new window.DOMParser().parseFromString(runner, "text/xml"))
+      .then(result => {
+        console.log(result)
+        // use XML data
+      }
+);
+```
+
+[![npm version](https://badge.fury.io/js/pocket-cors-rss-proxy.svg)](https://badge.fury.io/js/pocket-cors-rss-proxy)
--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const DomParser = require('dom-parser');
 
 const app = express();
 app.use(cors()); // using cors as middleware
+app.set('trust proxy', true); // allow grab request IP
 
 const port = 8001; // arbitrary 
 const parser = new DomParser();
 
-app.get('/source/*', (req, res, next) => {
+app.get('/source/*', (req, res) => {
     fetch(req.params[0])
         .then(res => res.text()) // get response text from fetching RSS URL
         .then(contents => parser.parseFromString(contents, "application/xml")) // parse XML from RSS URL into visible format
         .then(data => { // process fetched RSS data
             res.send(data);
+            console.log(`Request proxied! ✅\nRequest from: ${port} + ${req.ip} 📶, to proxy: ${req.params[0]}`);
         })
         .catch(e => {
-            console.error(e);
+            console.error(e + '❌');
         });
 });
 
 app.listen(port, () => {
-    console.log(`CORS RSS proxy running on ${port}`)
+    console.log(`CORS RSS proxy running on port ${port}`)
 });
